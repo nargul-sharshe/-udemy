cdb3bad1702e5efaad7cbf00a6af37999953129f
Nargul <nargul.sharshe@gmail.com>
2023-01-27 09:08:26 +0600

    циклы и условия

--- js/script.js
+++ js/script.js
@@ -23,10 +23,102 @@
 // 24.01.2023
 // 1)
 
+// const numberOfFilms = + prompt("Сколько фильмов вы уже посмотрели?", "");
+// console.log(numberOfFilms);
+
+//  2)
+
+// const personalMovieDB = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// 3)
+
+    // const a = prompt("Один из последних просмотренных фильмов?", ""),
+    //       b = prompt("На сколко оцените его?", ""),
+    //       c = prompt("Один из последних просмотренных фильмов?", ""),
+    //       d = prompt("Один из последних просмотренных фильмов?", "");
+
+
+    // personalMovieDB.movies[a] = b;
+    // personalMovieDB.movies[c] = d;
+
+    // console.log(personalMovieDB);
+
+//  Условия
+
+// if (4 == 9){
+//     console.log('OK!');
+// } else {
+//     console.log ('Error');
+// }
+
+
+
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log ('Ok');
+// }
+  
+// // Тернарный оператор
+
+// (num === 50) ? console.log('Ok') : console.log('Error');
+
+
+// const num = 50;
+// switch (num) {
+//     case 49:
+//         console.log('Неверно');
+//         break;
+//     case 100:
+//         console.log('Неверно');
+//         break;
+//     case 50:
+//         console.log('В точку');
+//         break;
+//     default:
+//         console.log('Не в этот раз');
+// }
+
+// Циклы
+
+    // let num = 50;
+
+    // while (num < 55) {
+    //     console.log(num);
+    //     num++;
+    // }
+
+    // do {
+    //     console.log(num);
+    //     num++;
+    // }
+    // while (num < 55);
+
+    // for (let i = 1; i < 8; i++) {
+    //     if (i === 6) {
+    //         // break;
+    //         continue;
+    //     }
+    //     console.log(i);
+    // }
+
+// Задача 2
+
+// 1)
+
+
 const numberOfFilms = + prompt("Сколько фильмов вы уже посмотрели?", "");
 console.log(numberOfFilms);
 
-// 2)
 
 const personalMovieDB = {
     count: numberOfFilms,
@@ -36,18 +128,30 @@ const personalMovieDB = {
     privat: false
 };
 
-// 3)
 
+for (let i = 0; i < 2; i++) {
     const a = prompt("Один из последних просмотренных фильмов?", ""),
-            b = prompt("На сколко оцените его?", ""),
-            c = prompt("Один из последних просмотренных фильмов?", ""),
-            d = prompt("Один из последних просмотренных фильмов?", "");
+          b = prompt("На сколко оцените его?", "");
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+}
 
 
-    personalMovieDB.movies[a] = b;
-    personalMovieDB.movies[c] = d;
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
 
-    console.log(personalMovieDB);
-
-
-
+console.log(personalMovieDB);
